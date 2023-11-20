58b80bfbfcef95cd64bad72c31ff9fe4b4ef3725
Rubendvb <rubenvasquez556@gmail.com>
2023-11-20 00:34:17 -0300

    fix spacing

--- src/pages/Community/Community.jsx
+++ src/pages/Community/Community.jsx
@@ -8,7 +8,36 @@ import './Community.scss'
 
 export default function Community() {
   return (
-    <section>
+    <section className="community">
+      <article className="communityCard">
+        <CardCode />
+
+        <div className="communityCard__header">
+          <h2>Título do projeto</h2>
+
+          <p>Essa é a descrição do meu projeto.</p>
+        </div>
+
+        <div className="communityCard__footer">
+          <div className="communityCard__footer__info">
+            <div>
+              <img src={Comment} alt="" />
+              <span>9</span>
+            </div>
+
+            <div>
+              <img src={Heart} alt="" />
+              <span>9</span>
+            </div>
+          </div>
+
+          <div className="communityCard__footer__profile">
+            <img src={Photo} alt="" />
+            <span>@Harry</span>
+          </div>
+        </div>
+      </article>
+
       <article className="communityCard">
         <CardCode />
 
